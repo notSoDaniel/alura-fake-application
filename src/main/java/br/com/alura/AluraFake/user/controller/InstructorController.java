cd756d465c45caf8d119ed636687c1ac2773511d
Victor Daniel <victudanieu@gmail.com>
2025-10-04 18:32:19 -0300

    feat: Implement instructor report endpoint with error handling

--- src/main/java/br/com/alura/AluraFake/user/controller/InstructorController.java
+++ src/main/java/br/com/alura/AluraFake/user/controller/InstructorController.java
@@ -1,6 +1,10 @@
 package br.com.alura.AluraFake.user.controller;
 
+import br.com.alura.AluraFake.user.dto.InstructorReportDTO;
 import br.com.alura.AluraFake.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,9 @@ public class InstructorController {
         this.userService = userService;
     }
 
+    @GetMapping("/{id}/courses")
+    public ResponseEntity<InstructorReportDTO> getInstructorReport(@PathVariable Long id) {
+        InstructorReportDTO report = userService.generateInstructorReport(id);
+        return ResponseEntity.ok(report);
+    }
 }
