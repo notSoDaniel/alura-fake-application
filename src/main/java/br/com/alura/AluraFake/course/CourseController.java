64f79b5945fec1fd4047b5c89de3c602588d4e1b
Victor Daniel <victudanieu@gmail.com>
2025-10-04 17:08:36 -0300

    feat: Implement course publication endpoint and logic

--- src/main/java/br/com/alura/AluraFake/course/CourseController.java
+++ src/main/java/br/com/alura/AluraFake/course/CourseController.java
@@ -1,5 +1,6 @@
 package br.com.alura.AluraFake.course;
 
+import br.com.alura.AluraFake.course.service.CourseService;
 import br.com.alura.AluraFake.user.*;
 import br.com.alura.AluraFake.util.ErrorItemDTO;
 import jakarta.validation.Valid;
@@ -15,18 +16,18 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
     private final UserRepository userRepository;
+    private final CourseService courseService;
 
     @Autowired
-    public CourseController(CourseRepository courseRepository, UserRepository userRepository){
+    public CourseController(CourseRepository courseRepository, UserRepository userRepository, CourseService courseService){
         this.courseRepository = courseRepository;
         this.userRepository = userRepository;
+        this.courseService = courseService;
     }
 
     @Transactional
     @PostMapping("/course/new")
     public ResponseEntity createCourse(@Valid @RequestBody NewCourseDTO newCourse) {
-
-        //Caso implemente o bonus, pegue o instrutor logado
         Optional<User> possibleAuthor = userRepository
                 .findByEmail(newCourse.getEmailInstructor())
                 .filter(User::isInstructor);
@@ -43,7 +44,7 @@ public class CourseController {
     }
 
     @GetMapping("/course/all")
-    public ResponseEntity<List<CourseListItemDTO>> createCourse() {
+    public ResponseEntity<List<CourseListItemDTO>> listCourses() {
         List<CourseListItemDTO> courses = courseRepository.findAll().stream()
                 .map(CourseListItemDTO::new)
                 .toList();
@@ -51,8 +52,8 @@ public class CourseController {
     }
 
     @PostMapping("/course/{id}/publish")
-    public ResponseEntity createCourse(@PathVariable("id") Long id) {
+    public ResponseEntity publishCourse(@PathVariable("id") Long id) {
+        courseService.publishCourse(id);
         return ResponseEntity.ok().build();
     }
-
 }
